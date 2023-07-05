9c759423b40f34015d3aad4f46761c72df047c95
Mateus Moreira <mateusmoreira090@hotmail.com>
2023-07-05 14:30:31 -0300

    Layout correto App Lista Vip com ids

--- AppListaCurso/app/src/main/java/devandroid/moreira/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/moreira/applistacurso/model/Pessoa.java
@@ -2,11 +2,37 @@ package devandroid.moreira.applistacurso.model;
 
 public class Pessoa {
 
+     String primeiroNome;
+     String sobreNome;
+     String cursoDesejado;
+     String telefoneContato;
+
     public Pessoa(){
 
     }
 
-
-
-
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+    public String getSobreNome() {
+        return sobreNome;
+    }
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
 }
--- AppListaCurso/app/src/main/java/devandroid/moreira/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/moreira/applistacurso/view/MainActivity.java
@@ -9,12 +9,22 @@ import devandroid.moreira.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
+    String dadosPessoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
      pessoa = new Pessoa();
+     pessoa.setPrimeiroNome("Mateus");
+     pessoa.setSobreNome("Moreira");
+     pessoa.setCursoDesejado("Android");
+     pessoa.setTelefoneContato("11-898765");
+
+     dadosPessoa = pessoa.getCursoDesejado();
+
+     toString()
+
 
 
 
